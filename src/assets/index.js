c84aeb45fa7af28169ffb7665020c112285574a3
SamstyleGhost <rohancrrm@gmail.com>
2024-07-04 01:47:11 +0530

    Bruh

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,3 @@
-import profile_pic from './profile_pic.jpg';
 import anime from './anime.jpeg';
 import gmail from './gmail.svg';
 import github from './github.svg';
@@ -6,7 +5,6 @@ import linkedin from './linkedin.svg';
 import arrow from './arrow.svg';
 
 export {
-  profile_pic,
   gmail,
   github,
   linkedin,
